086cdaff33c9ab792fc943c501dade1dc859e447
Maciejlys <maciej.lyszczarz98@gmail.com>
2021-11-20 18:59:37 +0100

    fixes

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -9,17 +9,23 @@ export const Counter: React.FC<SingleCounter> = ({
   dueTime,
 }) => {
   const [targetTime, setTargetTime] = useState([0, 0, 0, 0]);
+  const [expired, setExpired] = useState(true);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    let interval = setInterval(async () => {
       let newDate = countDownInterval(dueDate, dueTime);
-      setTargetTime(newDate);
+      await setTargetTime(newDate);
     }, 1000);
     return () => clearInterval(interval);
   }, [id, dueDate, dueTime]);
 
+  console.log("XD");
+
   const [days, hours, minutes, seconds] = targetTime;
 
+  {
+    expired && <h1>Expired</h1>;
+  }
   return (
     <h1>
       {days}d {hours}h {minutes}m {seconds}s
