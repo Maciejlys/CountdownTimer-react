669d8d0cd51af12000e968e15a344ae6cf5f610f
Maciejlys <maciej.lyszczarz98@gmail.com>
2021-11-21 13:26:32 +0100

    loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Counters } from "./Components/Counters";
+import { Loading } from "./Components/Loading";
 import { Navbar } from "./Components/Navbar";
 import data from "./data/data.json";
 
@@ -12,14 +13,29 @@ function App() {
   const [counters, setCounters] = useState(data);
   const [names, setNames] = useState([""]);
   const [index, setIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setNames(getNames());
     setCounters(data);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
   }, []);
 
   return (
     <main>
+      <main
+        className="load"
+        style={{
+          display: isLoading ? "flex" : "none",
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          zIndex: "2",
+        }}>
+        <Loading />
+      </main>
       <Navbar names={names} setIndex={setIndex} current={index} />
       <Counters counters={counters} index={index} />
     </main>
